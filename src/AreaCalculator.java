5c9c19770f7e4aa91e7b7efe14399975db6a76eb
Davian Affognon <affogdav000@warren.k12.in.us>
2024-09-05 11:04:53 -0400

    yeahyeahyeah

--- src/AreaCalculator.java
+++ src/AreaCalculator.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class AreaCalculator {
+class Main {
+
+    public static void main(String[] args) {
+        System.out.println(area(5.0));
+        System.out.println(area(-1));
+        System.out.println(area(5.0, 4.0));
+        System.out.println(area(-1.0, 4.0));
+
+    }
+
+    public static double area(double x, double y) {
+        if (x < 0 || y < 0) {
+            return -1.0;
+        }
+        return (x * y);
+    }
+
+    private static double area(double radius) {
+        if (radius < 0) {
+            return -1.0;
+        }
+        return (Math.PI * radius * radius);
+        // use Math.PI for PI
+    }
+
 }
